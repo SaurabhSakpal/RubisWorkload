90ac502f091dcd87241ae2ea6aa5c296ffe8ea25
Your Name <you@example.com>
2017-04-25 21:03:20 -0400

    Added default transitions

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd bin
 # in milliseconds
-java -cp . edu/rice/rubis/client/WorkloadGenerator demo1 600000
+java -cp . edu/rice/rubis/client/WorkloadGenerator curve_nasa_60_2 300000
 cd ../
